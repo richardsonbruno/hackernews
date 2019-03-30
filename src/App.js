d16990c55abb6bcddaa2ba31b35a888dbc74d952
Richardson Bruno <richardsonbrc@MacBook-Pro-de-Richardson.local>
2019-03-30 18:42:58 -0300

    Listando os itens na página

--- src/App.js
+++ src/App.js
@@ -1,12 +1,43 @@
 import React, { Component } from 'react';
 
-class App extends Component {
-    render(){
-        const helloWorld = "Seja bem vindo ao livro Road to React";
+const list = [
+    {
+        title: 'React',
+        url: 'https://facebook.github.io/react',
+        author: 'Jordan Walke',
+        num_comments: 3,
+        points: 4,
+        objectID: 0
+    },
+    {
+        title: 'React Native',
+        url: 'https://facebook.github.io/react-native',
+        author: 'Jordan Walke',
+        num_comments: 10,
+        points: 2,
+        objectID: 1
+    },
+];
 
+class App extends Component {
+
+    render(){
         return (
-            <div>
-                <h1>{helloWorld}</h1>
+            <div className='App'>
+                {list.map(function(item, key){
+                    return (
+                        <div key={key}>
+                            <span>
+                                <a href={item.url} title={item.title}>
+                                    {item.title}
+                                </a>
+                            </span>
+                            <span>{item.author}</span>
+                            <span>{item.num_comments}</span>
+                            <span>{item.points}</span>
+                        </div>
+                    );
+                })}
             </div>
         );
     }
